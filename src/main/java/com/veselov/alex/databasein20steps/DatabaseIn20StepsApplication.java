820e2420901e6456b3e0ff4f9018d7e194220836
Pogorelov Roman <rmk140987@gmail.com>
2019-12-06 09:44:07 +0300

    Initial commit

---
+++ src/main/java/com/veselov/alex/databasein20steps/DatabaseIn20StepsApplication.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.databasein20steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseIn20StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseIn20StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veselov/alex/databasein20steps/DatabaseIn20StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.databasein20steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseIn20StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
